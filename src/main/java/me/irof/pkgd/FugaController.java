6254ae8d1be134fbf3a021456633de119611c8cc
irof <irof.ocean@gmail.com>
2021-09-14 12:38:13 +0900

    Hoge,FugaからPiyoを使う

--- src/main/java/me/irof/pkgd/FugaController.java
+++ src/main/java/me/irof/pkgd/FugaController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class FugaController {
     FugaService service;
+    PiyoService piyoService;
 
     public FugaController(FugaService service) {
         this.service = service;
--- src/main/java/me/irof/pkgd/HogeController.java
+++ src/main/java/me/irof/pkgd/HogeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 public class HogeController {
 
     HogeService service;
+    PiyoService piyoService;
 
     public HogeController(HogeService service) {
         this.service = service;
