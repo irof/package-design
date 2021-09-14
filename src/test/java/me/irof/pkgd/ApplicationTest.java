73a309c3783aeb631b70794930c126694cd81a4d
irof <irof.ocean@gmail.com>
2021-09-14 12:19:03 +0900

    entrypoint

---
+++ src/test/java/me/irof/pkgd/ApplicationTest.java
@@ -0,0 +1,12 @@
+package me.irof.pkgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void bootUp() throws Exception {
+    }
+}
